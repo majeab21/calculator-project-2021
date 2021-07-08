7744561933d2a8674289f2ea5ca41c3f9ae92bf6
majeab21 <majea.bailey0@gmail.com>
2021-07-08 18:11:45 +0000

    Added if statements

---
+++ calorie-calculator.py
@@ -0,0 +1,21 @@
+# Have a welcome statement - print("Welcome to the calorie calculator")
+print("Welcome to the calorie calculator")
+
+# Ask if they are Male or Female 
+gender = input("Are you Male or Female?")
+
+if gender == ("Male"):
+    msg = "The recommended calorie intake for your group is around 2500. This may vary based on your height/weight/age. "
+elif gender == ("female"):
+    msg = "The recommended calorie intake for yourgroup is 2,000. This may vary based on your height/weight/age. "
+    # return msg
+    print(msg)
+
+
+
+# Tell them that this is the amount of calories that a reptuable source recommends for them
+# 2,000 calories a day for women and 2,500 for men.
+
+# Do research to figure out the general amount of calories each food group has: fruits, vegetables, grains, nuts, meats, dairy)
+
+#ask them  
